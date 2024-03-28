3abade661a93b67ceb4e5a9b2dacf00e57f6d45d
Ihor Mokrov <60344728+IhorMokrov@users.noreply.github.com>
2024-03-28 14:56:02 +0200

    api

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-function fetchNews() {
-  const apiUrl = "https://www.pravda.com.ua/news/date_09032024/";
+async function getAPIresponse() {
+  // NASA API-key:
+  const div = document.querySelector(".container");
 
-  let myFetch = fetch(apiUrl);
-  console.table(myFetch);
+  const apiKey = "OaPinqeq6WhflrHOHkq5dJ2mFVravvKkjEC1gJXu";
+  const apiUrl = `https://api.nasa.gov/planetary/earth/assets?lon=-95.33&lat=29.78&date=2018-01-01&&dim=0.10&api_key=${apiKey}`;
+
+  let result = await fetch(apiUrl);
+  let resultWait = await result.json();
+  console.log(resultWait);
+
+  const { url } = resultWait;
+  // console.log(url);
+
+  div.innerHTML = `<img class="srcImg" src="${url}" alt=""></img>`;
+
+  return url;
 }
 
-fetchNews();
+getAPIresponse();
 
-// Access to fetch at 'https://www.pravda.com.ua/news/date_09032024/' from origin 'http://127.0.0.1:5500' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+// async function showAvatar() {
+//   // запрашиваем JSON с данными пользователя
+//   const apiKey = "OaPinqeq6WhflrHOHkq5dJ2mFVravvKkjEC1gJXu";
+//   const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+//   let response = await fetch(apiUrl);
+//   let user = await response.json();
 
-//   fetch(apiUrl)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Network response was not ok");
-//       }
-//       console.log("OK-1");
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log("OK-2");
-//       console.log(data);
-//     })
-//     .catch((error) => {
-//       console.error("НАКРЫЛОСЬ:", error);
-//     });
+//   console.log(response);
+//   console.log(user.hdurl);
+
+//   // запрашиваем информацию об этом пользователе из github
+//   let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+//   let githubUser = await githubResponse.json();
+
+//   // отображаем аватар пользователя
+//   let img = document.createElement("img");
+//   img.src = githubUser.avatar_url;
+//   img.className = "promise-avatar-example";
+//   document.body.append(img);
+
+//   // ждём 3 секунды и затем скрываем аватар
+//   await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+//   img.remove();
+
+//   return githubUser;
+// }
+
+// showAvatar();
