4f7145904dce7ef6b724eceacfe88843171ce20d
Ihor Mokrov <60344728+IhorMokrov@users.noreply.github.com>
2024-03-29 21:49:15 +0200

    12dd

--- script.js
+++ script.js
@@ -13,16 +13,32 @@ async function getAPIresponse() {
   // const apiKey = "4f560c6d87f9572b10f95d7b21a37b7c";
   // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-  let result = await fetch(apiUrl);
-  let resultWait = await result.json();
-  console.log(resultWait);
+  // calendarific.com
+  const headers = {
+    Authorization: "dd57d7523b80e08db3fe0bddd5322787",
+  };
+  const YOUR_APP_TOKEN = "dd57d7523b80e08db3fe0bddd5322787";
+  const apiUrl = `https://favqs.com/api/quotes/`;
+  // https://calendarific.com/api/v2/holidays?api_key=baa9dc110aa712sd3a9fa2a3dwb6c01d4c875950dc32vs
+  //https://calendarific.com/api/v2/holidays?api_key=ShPnI4JwZuWu9iIT2L2PXkr7cqFiO2PZ
+  let result = await fetch(apiUrl, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token token=dd57d7523b80e08db3fe0bddd5322787`,
+      "If-None-Match": "558f0b9bea2e6910b1f93de7f4c0d47c",
+    },
+  });
+  console.log(result);
 
-  const { url } = resultWait;
+  // let result = await fetch(apiUrl);
+  // let resultWait = await result.json();
+
+  // const { url } = resultWait;
   // console.log(url);
 
-  div.innerHTML = `<img class="srcImg" src="${url}" alt=""></img>`;
+  // div.innerHTML = `<img class="srcImg" src="${url}" alt=""></img>`;
 
-  return url;
+  // return url;
 }
 
 getAPIresponse();
